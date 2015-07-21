cdbaae006f6f01571d6665df6300639b2c6a7cb2
AlanTai <rainman.tai@gmail.com>
2015-07-21 15:54:17 -0700

    update crawler

--- handle_bitcoin_rate_update_data.js
+++ handle_bitcoin_rate_update_data.js
@@ -2,15 +2,15 @@ var request = require('request');
 request
   .get('http://www.moneysedge.com')
   .on('response', function(response) {
-    	console.log(response.headers)
-	  	console.log(response.headers['set-cookie'])
+	  console.log(response.headers);
+	  console.log(response.headers['set-cookie']);
+	  
+	  //
+	  request.post({
+	  	url:'http://www.moneysedge.com/update_bitcoin_rate',
+	  	jar: cookie_jar,
+	  	form: { bitcoin_exchange_rate : { key : 'bitcoin_rate', value : '$274.28' } }}, function(err,httpResponse,body){
+	  		console.log(body);
+	  	});
   });
-var cookie_jar = request.jar();
   
-
-request.post({
-	url:'http://www.moneysedge.com/update_bitcoin_rate',
-	jar: cookie_jar,
-	form: { bitcoin_exchange_rate : { key : 'bitcoin_rate', value : '$274.28' } }}, function(err,httpResponse,body){
-		console.log(body);
-	});

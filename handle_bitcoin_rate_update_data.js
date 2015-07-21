e03b06f2b8bcacd99315755763d890b2168b311e
AlanTai <rainman.tai@gmail.com>
2015-07-21 16:24:21 -0700

    update crawler

--- handle_bitcoin_rate_update_data.js
+++ handle_bitcoin_rate_update_data.js
@@ -2,12 +2,12 @@ var request = require('request');
 request
   .get('http://www.moneysedge.com')
   .on('response', function(response) {
-	  console.log(response.headers);
-	  console.log(response.headers['set-cookie'][0]);
+	  console.log(response);
+	  // console.log(response.headers['set-cookie'][0]);
 	  
 	  var crsf_token = response.headers['set-cookie'][0];
 	  crsf_token = crsf_token.substring(crsf_token.indexOf('=') + 1, crsf_token.indexOf(';'));
-	  console.log(crsf_token);
+	  // console.log(crsf_token);
 	  
 	  // request
 	  request.post({

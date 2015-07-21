35c496f9a6ed54a228904f8dc5dc222dcebe0297
AlanTai <rainman.tai@gmail.com>
2015-07-21 16:17:12 -0700

    update crawler

--- handle_bitcoin_rate_update_data.js
+++ handle_bitcoin_rate_update_data.js
@@ -7,6 +7,7 @@ request
 	  
 	  var crsf_token = response.headers['set-cookie'][0];
 	  crsf_token = crsf_token.substring(crsf_token.indexOf('=') + 1, crsf_token.indexOf(';'));
+	  console.log(crsf_token);
 	  
 	  // request
 	  request.post({

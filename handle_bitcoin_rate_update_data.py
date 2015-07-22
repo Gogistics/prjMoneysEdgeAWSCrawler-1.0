b2620957d5bcf1c05210ed1814820ef2c7d67318
AlanTai <rainman.tai@gmail.com>
2015-07-22 14:03:18 -0700

    update daexchange mechanism

--- handle_bitcoin_rate_update_data.py
+++ handle_bitcoin_rate_update_data.py
@@ -3,7 +3,7 @@ import urllib.parse as urllib_parser
 
 def update_data():
     url = 'http://www.moneysedge.com/update_bitcoin_rate'
-    values = { 'bitcoin_exchange_rate' : '!alantai_*bitcoin_%analysis'
+    values = { 'bitcoin_exchange_rate' : '!alantai_*bitcoin_%analysis',
             'bitfinex' : '$267.45'}
             
     data = urllib_parser.urlencode(values)

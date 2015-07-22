00d94739b26f817c246e8fce28d479e3b0090b28
AlanTai <rainman.tai@gmail.com>
2015-07-21 17:00:46 -0700

    update crawler

--- handle_bitcoin_rate_update_data.js
+++ handle_bitcoin_rate_update_data.js
@@ -1,7 +1,6 @@
 var request = require('request');
-request
-  .get('http://www.moneysedge.com/update_bitcoin_rate?bitcoin_exchange_rate=test')
-  .on('response', function(error, response, body) {
-	  console.log(response);
-  });
-  
+request('http://moneysedge.com/update_bitcoin_rate?bitcoin_exchange_rate=qwdwdwe', function (error, response, body) {
+  	if (!error && response.statusCode == 200) {
+    	console.log(body) // Show the HTML for the Google homepage.
+  	}
+});

98323c57110c5c748a4d15228eb563de1322cb2d
AlanTai <rainman.tai@gmail.com>
2015-07-22 11:10:53 -0700

    update daexchange mechanism

--- handle_bitcoin_rate_update_data.js
+++ handle_bitcoin_rate_update_data.js
@@ -1,5 +1,7 @@
 var request = require('request');
-request('http://moneysedge.com/update_bitcoin_rate?bitcoin_exchange_rate=qwdwdwe', function (error, response, body) {
+var request_str = '?bitcoin_exchange_rate=!alantai_*bitcoin_%analysis&bitfinext=$234.89&bitstamp=$267.32&btc_e=$287.21&btc_q=$267.98&bityes=256.73&coinbase=$278.33&coindesk=$276.38&itbit=$278.23&lakebtc=$254.38&okcoin=$233.87',
+	request_url = 'http://moneysedge.com/update_bitcoin_rate' + request_str;
+request( request_url, function (error, response, body) {
   	if (!error && response.statusCode == 200) {
     	console.log(body) // Show the HTML for the Google homepage.
   	}
